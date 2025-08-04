17a76fcf57e2016306b468e75b00caccdc8c4c6e
Edoardo Lova <edoardolova81@gmail.com>
2025-08-04 13:05:28 +0200

    feat: add modal form to post new review

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -5,6 +5,10 @@ export default function MovieDetailPage() {
     const { slug } = useParams();
     const [movie, setMovie] = useState(null);
     const [reviews, setReviews] = useState(null);
+    const [showModal, setShowModal] = useState(false); 
+    const [name, setName] = useState('');
+    const [text, setText] = useState('');
+    const [vote, setVote] = useState(1);
 
     useEffect(() => {
         fetch(`http://localhost:3000/movies/${slug}`)
@@ -18,6 +22,34 @@ export default function MovieDetailPage() {
     const formatDate = (dateString) => {
         const date = new Date(dateString);
         return date.toLocaleDateString('it-IT'); 
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newReview = {
+            movie_id: movie.id,  
+            name,
+            text,
+            vote,
+        };
+        console.log(newReview)
+
+        fetch(`http://localhost:3000/reviews/${slug}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newReview),
+        })
+        .then(res => res.json())
+        .then(data => {
+            setReviews([...reviews, data]);
+            setShowModal(false);
+            setName('');
+            setText('');
+            setVote(1);
+        });
     };
 
     return (
@@ -39,9 +71,12 @@ export default function MovieDetailPage() {
                 )}
             </div>
 
-            {/* Reviews Section */}
             <div className="mt-5">
-                <h3 className="mb-4">Reviews</h3>
+                <div className="d-flex mb-4 align-items-center">
+                    <h3 className="m-0 fs-2">Reviews</h3>
+                    <button className="btn ms-auto border-0 btn-add-review" onClick={() => setShowModal(true)}><i class="bi bi-plus-circle text-primary fs-3"></i></button>
+                </div>
+                {/* Review Section */}
                 <ul className="list-group list-group-flush">
                     {reviews?.map((review, index) => (
                         <li className="list-group-item d-flex justify-content-between align-items-center shadow-sm mb-4">
@@ -55,6 +90,45 @@ export default function MovieDetailPage() {
                     ))}
                 </ul>
             </div>
+
+            
+            {showModal && (
+                <div className="modal fade show" style={{  display: 'block',  backgroundColor: 'rgba(0, 0, 0, 0.84)', position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: 1000, }} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"onClick={() => setShowModal(false)}>
+                    <div  className="modal-dialog" style={{  position: 'relative',  zIndex: 1060  }} onClick={(e) => e.stopPropagation()}>
+                        <div className="modal-content bg-dark text-light">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Aggiungi Recensione</h5>
+                                <button type="button" className="btn-close" onClick={() => setShowModal(false)} aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="name" className="form-label">Nome</label>
+                                        <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="text" className="form-label">Recensione</label>
+                                        <textarea className="form-control" id="text" rows="4" value={text} onChange={(e) => setText(e.target.value)} required ></textarea>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="vote" className="form-label">Voto</label>
+                                        <select className="form-select" id="vote" value={vote} onChange={(e) => setVote(e.target.value)} required >
+                                            {[1, 2, 3, 4, 5].map(val => (
+                                                <option key={val} value={val}>{val}</option>
+                                            ))}
+                                        </select>
+                                    </div>
+                                    <div className="d-flex">
+                                        <button type="submit" className="btn btn-primary mx-auto" style={{ marginBottom: '20px' }} >
+                                            Invia Recensione
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
