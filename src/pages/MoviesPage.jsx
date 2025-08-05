4350a4744ea1a2697df26791166827579dd1a9a8
Edoardo Lova <edoardolova81@gmail.com>
2025-08-05 17:35:42 +0200

    feat: add MoviesCard component

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import slugify from 'slugify';
+
+import MoviesCard from "../components/MoviesCard";
 
 export default function MoviesPage(){
 
@@ -18,21 +18,7 @@ export default function MoviesPage(){
             <section className="container mb-5">
                 <h2 className="py-4">Film in evidenza</h2>
                 <div className="row gy-4">
-                    {movies?.map((movie) => (
-                        <div className="col col-md-4 col-lg-3" key={movie.id}>
-                            <Link to={`/movie/${slugify(movie.title, { lower: true, replacement: '-' })}`}>
-                                <div className="position-relative overflow-hidden rounded shadow movie-card">
-                                    <img src={movie.image} alt={movie.title} className="img-fluid w-100" style={{ objectFit: "cover", height: "400px" }} />
-                                    <div className="movie-overlay d-flex flex-column justify-content-end p-3">
-                                        <h5 className="text-white">{movie.title}</h5>
-                                        <p className="text-white-50 mb-0">{movie.director}</p>
-                                        <p className="text-white-50">{movie.release_year}</p>
-                                    </div>
-                                </div>
-                            
-                            </Link>
-                        </div>
-                    ))}
+                    <MoviesCard movies={movies}/>
                 </div>
             </section>
         </>
